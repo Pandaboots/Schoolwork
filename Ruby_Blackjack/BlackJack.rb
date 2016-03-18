13ea37e5a065b5a8c9a7e49e3cb5b2ad1e2d85e9
Pandaboots <Thomas_Tracy@student.uml.edu>
2016-03-18 15:04:03 -0400

    Need to finish handle aces

--- Ruby_Blackjack/BlackJack.rb
+++ Ruby_Blackjack/BlackJack.rb
@@ -4,7 +4,7 @@ require_relative 'View_application/View_Gameview'
 require_relative 'Controller/Controller'
 require_relative 'Model/Model_createDeck'
 
-Shoes.app width: 1024, height: 768 do
+Shoes.app title: "Blackjack!", width: 1024, height: 768 do
     background "#070"
     #newGame = Splash.new(self)
     #newGame.start_game
--- Ruby_Blackjack/View_application/View_Gameview.rb
+++ Ruby_Blackjack/View_application/View_Gameview.rb
@@ -24,6 +24,8 @@ class GameView
     #create the game view for the game to be played in
     def create_playerSpace
 
+        @playerWinsCount = 0
+        @dealerWinsCount = 0
         #set up the gamescreen with the deck and the dealer and the player
         #outside stack holding everything
         @gameStack = @shoes.stack do
@@ -31,10 +33,13 @@ class GameView
             @dealerFlow = @shoes.flow(height: 250) do
                 #stack holding relevant information about the dealer
                 @dealerInfo = @shoes.stack do
-                    @shoes.para("Dealer ")
+                  @dealerName =  @shoes.para("Dealer ")
                     @dealerScore = @shoes.para("??")
-                    @dealerWins = @shoes.para("Wins:")
+                    @dealerWins = @shoes.para("Wins: #{@dealerWinsCount}")
                 end# dealerInfo
+                @cardflowDealer = @shoes.flow do
+                  @cardArDealer = Array.new
+                end
             end# dealerFlow
 
             #Computer Player?
@@ -43,44 +48,134 @@ class GameView
             @playerFLow = @shoes.flow(height: 250) do
                 #stack holding relevant player information
                 @playerInfo = @shoes.stack do
-                    @shoes.para("Player ")
-                    @playerScore = @shoes.para("??")
-                    @playerWins = @shoes.para("Wins: ")
+                  @name =  @shoes.para "Player "
+                    @playerScore = @shoes.para "??"
+                    @playerWins = @shoes.para "Wins: #{@playerWinsCount}"
                 end# playerInfo
                 @cardflowPlayer = @shoes.flow do
-
+                  @cardArPlayer = Array.new
                 end
             end# playerflow
         end# gameStack
 
         #make room for the control buttons for the player to use to play the game
         @controlFlow = @shoes.flow do
-            @hit = @shoes.button("Draw");
-            @stay = @shoes.button("stay");
-            @newGame = @shoes.button("New Game");
+            @newGame = @shoes.button("New Game")
+            @hit = @shoes.button("Draw")
+            @stay = @shoes.button("stay")
+            @hit.hide
+            @stay.hide
         end# controlFlow
+
+        #and the winner banner
+        @winnerFlow = @shoes.flow do
+
+        end
 
 
         #handle the starting of a new game
         @newGame.click do
+          new_game_start
+       end
 
+       #handle when the player hits
+       @hit.click do
+         @controller.draw_card(@playerHand)
+         @cardflowPlayer.append do
+           @cardArPlayer << @shoes.image("Cards/#{@playerHand.get_hand_name(-1)}.png",width:  $CARD_WIDTH, height: $CARD_HEIGHT)
+         end
 
-            @controller.shuffle_deck
-            @controller.draw_card(@playerHand)
-            @controller.draw_card(@playerHand)
-            @cardflowPlayer.append do
-                i = 0
-                while(i < @playerHand.get_hand_size)
+         @playerScore.clear
+         @playerInfo.after(@name) do
+           @playerScore = @shoes.para("#{@playerHand.get_hand_score}")
+         end
 
-                    @playerCards.push( @shoes.image("Cards/#{@playerHand.get_hand_name(i)}.png",width:  $CARD_WIDTH, height: $CARD_HEIGHT))
-                    i += 1
-                end
-            end
+         if(@playerHand.get_hand_score > 21)
+          @winnerFlow.append  do
+             @shoes.title "You Bust!", align: "center", stroke: "#FFF"
+           end
+           @dealerWinsCount += 1
+           @hit.hide
+           @stay.hide
+         end
+
+       end
+
+       #handle when the player decides to stay
+       @stay.click do
+         @hit.hide
+         @stay.hide
+         dealerScore = @dealerHand.handle_dealer(@controller)
+         i = 0
+         @cardArDealer.each { |x| x.clear  }
+         while( i < @dealerHand.get_hand_size)
+           @cardflowDealer.append do
+           @cardArDealer << @shoes.image("Cards/#{@dealerHand.get_hand_name(i)}.png",width:  $CARD_WIDTH, height: $CARD_HEIGHT)
+           end
+           i += 1
         end
 
+        @dealerScore.clear
+        @dealerInfo.after(@dealerName) do
+          @dealerScore = @shoes.para("#{@dealerHand.get_hand_score}")
+        end
+
+         if(dealerScore > @playerHand.get_hand_score || dealerScore == 1)
+           @winnerFlow.append do
+             @shoes.title "You Lose!", align: "center", stroke: "#FFF"
+           end
+           @dealerWinsCount += 1
+         else
+          @winnerFlow.append do
+             @shoes.title "You Win!", align: "center", stroke: "#FFF"
+           end
+           @playerWinsCount += 1
+         end
+       end
 
     end# create_playerSpace
 
+    #clearing the board of old cards and reloading the deck
+    def clear_board
+      @controller = GameController.new
+      @playerHand = Hand.new
+      @dealerHand = Hand.new
+      @cardArPlayer.each { |x| x.clear  }
+      @cardArDealer.each { |x| x.clear  }
+      @winnerFlow.clear
+      @dealerScore.clear
+      @dealerWins.clear
+      @dealerInfo.after(@dealerName) do
+        @dealerScore = @shoes.para "??"
+        @dealerWins = @shoes.para "Wins: #{@dealerWinsCount}"
+      end
+    end
 
+
+    #handling the new game
+    def new_game_start
+      @hit.show
+      @stay.show
+      clear_board
+      @controller.shuffle_deck
+      @controller.draw_card(@playerHand)
+      @controller.draw_card(@playerHand)
+      @controller.draw_card(@dealerHand)
+      @controller.draw_card(@dealerHand)
+      @cardflowPlayer.append do
+        @cardArPlayer << @shoes.image("Cards/#{@playerHand.get_hand_name(0)}.png",width:  $CARD_WIDTH, height: $CARD_HEIGHT)
+        @cardArPlayer << @shoes.image("Cards/#{@playerHand.get_hand_name(1)}.png",width:  $CARD_WIDTH, height: $CARD_HEIGHT)
+      end
+      @cardflowDealer.append do
+      @cardArDealer << @shoes.image("Cards/#{@dealerHand.get_hand_name(0)}.png",width:  $CARD_WIDTH, height: $CARD_HEIGHT)
+      @cardArDealer << @shoes.image("View_application/Card_Back.png",width:  $CARD_WIDTH, height: $CARD_HEIGHT)
+      end
+      @playerScore.clear
+      @playerWins.clear
+      @playerInfo.after(@name) do
+        @playerScore = @shoes.para "#{@playerHand.get_hand_score}"
+        @playerWins = @shoes.para "Wins: #{@playerWinsCount}"
+      end
+    end
 
 end# GameView
